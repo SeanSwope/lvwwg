a98190e3aebd7bb7e829659ef90480ab3cc326cb
SeanSwope <Sean.Swope@comcast.net>
2022-01-30 20:48:49 -0500

    Add PrimeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem, PrimeNGConfig } from 'primeng/api';
+import { PrimeIcons } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'lvwwg';
+export class AppComponent implements OnInit {
+  title = 'Lehigh Valley Woodworkes Guild';
+  items!: MenuItem[];
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+      this.primengConfig.ripple = true;
+
+      this.items = [
+        {label: 'Update', icon: 'pi pi-refresh', command: () => { this.onSave(); }},
+        {label: 'Delete', icon: 'pi pi-times', command: () => { this.onSave(); }},
+        {label: 'Angular.io', icon: 'pi pi-info', url: 'http://angular.io'},
+        {separator: true},
+        {label: 'Setup', icon: 'pi pi-cog', routerLink: ['/setup']}
+    ];
+  }
+
+  onSave() {
+    alert('onSave() pressed');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule } from "@angular/forms";
+
+import { SplitButtonModule } from 'primeng/splitbutton';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    SplitButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
